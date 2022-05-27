2563f5171d02f848b0166636f0378b7c374bd8bc
John Paulo Panganiban <olopangs@gmail.com>
2022-05-28 06:35:18 +0800

    added register functionality API added validation in DTO level for register usename and password
    notes: save the password hash and password salt in the db using HMACSHA512()

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -19,14 +22,18 @@ namespace API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        // Action result is the interface/class that provides the BadRequest, Ok, etc.
+        // React terms is: const Register<Task<ActionResult<AppUser>>> = (registerDto: RegisterDtoProps)
+        // 
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
-            using var hmac = new System.Security.Cryptography.HMACSHA512();
+            if (await UserExists(registerDto.Username)) return BadRequest("User already exists");
+            using var hmac = new HMACSHA512();
 
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.Username,
+                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
             // add user
@@ -35,6 +42,19 @@ namespace API.Controllers
             // we call our database here and save the user to the users table
             await _context.SaveChangesAsync();
             return user;
+        }
+
+
+        // export const function helper in React terms 
+        // const UserExists = (username: string): boolean => {
+        // _context.User.AnyAsync returns boolean
+        // return await _context.Users.AnyAsync(x => x.UserName == username);
+
+        // create a variable inside the controller AccountController
+        // that can only be accessed to itself
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
