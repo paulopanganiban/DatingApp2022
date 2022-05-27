158453761fc0d0570ffc2aa709f920314ccfaaa6
John Paulo Panganiban <olopangs@gmail.com>
2022-05-28 06:35:18 +0800

    added register functionality API added validation in DTO level for register usename and password
    notes: save the password hash and password salt in the db using HMACSHA512()

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    // in React terms
+    // export interface RegisterDto { username: string, password: string }
+
+    public class RegisterDto
+    {
+        // using DTO is a good place for validations
+        // we are most likely working with the [ApiController] magic tricks
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
