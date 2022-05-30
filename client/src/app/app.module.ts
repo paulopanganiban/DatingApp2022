abb26763c324fe082e845abf3aab191b80064783
John Paulo Panganiban <olopangs@gmail.com>
2022-05-30 18:58:47 +0800

    added home register component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown'
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [
     // we declare here the components available to use
     AppComponent,
     NavComponent,
+    HomeComponent,
   ],
   // import angular modules
   imports: [
