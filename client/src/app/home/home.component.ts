d1a2be1865b519002cbddee0d8c70198bb478efb
John Paulo Panganiban <olopangs@gmail.com>
2022-06-01 12:35:54 +0800

    added fully working register functionality

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -8,24 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
-  users: any;
-  constructor(private http: HttpClient) { }
-
+  constructor() { }
   ngOnInit(): void {
-    this.getUsers();
   }
-
   registerToggle() {
     this.registerMode = !this.registerMode
   }
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      this.users = response;
-    }, error => {
-      console.log(error);
-    })
-  }
-
   cancelRegisterMode(event: boolean) {
     this.registerMode = event;
   }
