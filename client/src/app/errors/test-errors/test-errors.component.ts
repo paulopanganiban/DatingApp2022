92a3674c1f79070298b960e5af116961de7b77ea
John Paulo Panganiban <olopangs@gmail.com>
2022-06-07 12:02:50 +0800

    added eslint2019 for flat map

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestErrorsComponent implements OnInit {
   baseUrl = 'https://localhost:5001/api/'
+  validationErrors: string[] = [];
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
@@ -40,6 +41,8 @@ export class TestErrorsComponent implements OnInit {
     this.http.post(this.baseUrl + 'account/register', {})
       .subscribe
       (response => { console.log(response) },
-        error => { console.error(error) })
+        error => { console.error(error);
+        this.validationErrors = error
+        })
   }
 }
