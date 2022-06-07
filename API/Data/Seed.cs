15bec22bd86cd8371fc84caad39035d1fbf85839
John Paulo Panganiban <olopangs@gmail.com>
2022-06-07 18:53:01 +0800

    fixed not working seed data.
    lacking context users add and await context savechangesasync()

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -11,17 +11,23 @@ namespace API.Data
 {
     public class Seed
     {
+        private const string Path = "Data/UserSeedData.json";
+
         public static async Task SeedUsers(DataContext context)
         {
             if (await context.Users.AnyAsync()) return;
-            var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
-            var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+            var userData = await System.IO.File.ReadAllTextAsync(Path);
+              var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+            if (users == null) return;
             foreach (var user in users)
             {
                 using var hmac = new HMACSHA512();
                 user.UserName = user.UserName.ToLower();
                 user.PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes("password"));
+                user.PasswordSalt = hmac.Key;
+                context.Users.Add(user);
             }
+            await context.SaveChangesAsync();
         }
     }
 }
